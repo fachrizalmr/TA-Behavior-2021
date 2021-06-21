494593f193c3048cbf2d792d53be996998e91149
Muhammad Fachrizal Ramdani <32020922+fachrizalmr@users.noreply.github.com>
2021-06-21 17:38:56 +0800

    new

---
+++ Modeling/waktu.py
@@ -0,0 +1,866 @@
+def cekWaktu(j, m):
+    if j == 0 and m >= 0 and m <= 5:
+        now = 1
+
+    elif j == 0 and m >= 5 and m <= 10:
+        now = 2
+
+    elif j == 0 and m >= 10 and m <= 15:
+        now = 3
+
+    elif j == 0 and m >= 15 and m <= 20:
+        now = 4
+
+    elif j == 0 and m >= 20 and m <= 25:
+        now = 5
+
+    elif j == 0 and m >= 25 and m <= 30:
+        now = 6
+
+    elif j == 0 and m >= 30 and m <= 35:
+        now = 7
+
+    elif j == 0 and m >= 35 and m <= 40:
+        now = 8
+
+    elif j == 0 and m >= 40 and m <= 45:
+        now = 9
+
+    elif j == 0 and m >= 45 and m <= 50:
+        now = 10
+
+    elif j == 0 and m >= 50 and m <= 55:
+        now = 11
+
+    elif j == 0 and m >= 55 and m <= 0:
+        now = 12
+
+    elif j == 1 and m >= 0 and m <= 5:
+        now = 13
+
+    elif j == 1 and m >= 5 and m <= 10:
+        now = 14
+
+    elif j == 1 and m >= 10 and m <= 15:
+        now = 15
+
+    elif j == 1 and m >= 15 and m <= 20:
+        now = 16
+
+    elif j == 1 and m >= 20 and m <= 25:
+        now = 17
+
+    elif j == 1 and m >= 25 and m <= 30:
+        now = 18
+
+    elif j == 1 and m >= 30 and m <= 35:
+        now = 19
+
+    elif j == 1 and m >= 35 and m <= 40:
+        now = 20
+
+    elif j == 1 and m >= 40 and m <= 45:
+        now = 21
+
+    elif j == 1 and m >= 45 and m <= 50:
+        now = 22
+
+    elif j == 1 and m >= 50 and m <= 55:
+        now = 23
+
+    elif j == 1 and m >= 55 and m <= 0:
+        now = 24
+
+    elif j == 2 and m >= 0 and m <= 5:
+        now = 25
+
+    elif j == 2 and m >= 5 and m <= 10:
+        now = 26
+
+    elif j == 2 and m >= 10 and m <= 15:
+        now = 27
+
+    elif j == 2 and m >= 15 and m <= 20:
+        now = 28
+
+    elif j == 2 and m >= 20 and m <= 25:
+        now = 29
+
+    elif j == 2 and m >= 25 and m <= 30:
+        now = 30
+
+    elif j == 2 and m >= 30 and m <= 35:
+        now = 31
+
+    elif j == 2 and m >= 35 and m <= 40:
+        now = 32
+
+    elif j == 2 and m >= 40 and m <= 45:
+        now = 33
+
+    elif j == 2 and m >= 45 and m <= 50:
+        now = 34
+
+    elif j == 2 and m >= 50 and m <= 55:
+        now = 35
+
+    elif j == 2 and m >= 55 and m <= 0:
+        now = 36
+
+    elif j == 3 and m >= 0 and m <= 5:
+        now = 37
+
+    elif j == 3 and m >= 5 and m <= 10:
+        now = 38
+
+    elif j == 3 and m >= 10 and m <= 15:
+        now = 39
+
+    elif j == 3 and m >= 15 and m <= 20:
+        now = 40
+
+    elif j == 3 and m >= 20 and m <= 25:
+        now = 41
+
+    elif j == 3 and m >= 25 and m <= 30:
+        now = 42
+
+    elif j == 3 and m >= 30 and m <= 35:
+        now = 43
+
+    elif j == 3 and m >= 35 and m <= 40:
+        now = 44
+
+    elif j == 3 and m >= 40 and m <= 45:
+        now = 45
+
+    elif j == 3 and m >= 45 and m <= 50:
+        now = 46
+
+    elif j == 3 and m >= 50 and m <= 55:
+        now = 47
+
+    elif j == 3 and m >= 55 and m <= 0:
+        now = 48
+
+    elif j == 4 and m >= 0 and m <= 5:
+        now = 49
+
+    elif j == 4 and m >= 5 and m <= 10:
+        now = 50
+
+    elif j == 4 and m >= 10 and m <= 15:
+        now = 51
+
+    elif j == 4 and m >= 15 and m <= 20:
+        now = 52
+
+    elif j == 4 and m >= 20 and m <= 25:
+        now = 53
+
+    elif j == 4 and m >= 25 and m <= 30:
+        now = 54
+
+    elif j == 4 and m >= 30 and m <= 35:
+        now = 55
+
+    elif j == 4 and m >= 35 and m <= 40:
+        now = 56
+
+    elif j == 4 and m >= 40 and m <= 45:
+        now = 57
+
+    elif j == 4 and m >= 45 and m <= 50:
+        now = 58
+
+    elif j == 4 and m >= 50 and m <= 55:
+        now = 59
+
+    elif j == 4 and m >= 55 and m <= 0:
+        now = 60
+
+    elif j == 5 and m >= 0 and m <= 5:
+        now = 61
+
+    elif j == 5 and m >= 5 and m <= 10:
+        now = 62
+
+    elif j == 5 and m >= 10 and m <= 15:
+        now = 63
+
+    elif j == 5 and m >= 15 and m <= 20:
+        now = 64
+
+    elif j == 5 and m >= 20 and m <= 25:
+        now = 65
+
+    elif j == 5 and m >= 25 and m <= 30:
+        now = 66
+
+    elif j == 5 and m >= 30 and m <= 35:
+        now = 67
+
+    elif j == 5 and m >= 35 and m <= 40:
+        now = 68
+
+    elif j == 5 and m >= 40 and m <= 45:
+        now = 69
+
+    elif j == 5 and m >= 45 and m <= 50:
+        now = 70
+
+    elif j == 5 and m >= 50 and m <= 55:
+        now = 71
+
+    elif j == 5 and m >= 55 and m <= 0:
+        now = 72
+
+    elif j == 6 and m >= 0 and m <= 5:
+        now = 73
+
+    elif j == 6 and m >= 5 and m <= 10:
+        now = 74
+
+    elif j == 6 and m >= 10 and m <= 15:
+        now = 75
+
+    elif j == 6 and m >= 15 and m <= 20:
+        now = 76
+
+    elif j == 6 and m >= 20 and m <= 25:
+        now = 77
+
+    elif j == 6 and m >= 25 and m <= 30:
+        now = 78
+
+    elif j == 6 and m >= 30 and m <= 35:
+        now = 79
+
+    elif j == 6 and m >= 35 and m <= 40:
+        now = 80
+
+    elif j == 6 and m >= 40 and m <= 45:
+        now = 81
+
+    elif j == 6 and m >= 45 and m <= 50:
+        now = 82
+
+    elif j == 6 and m >= 50 and m <= 55:
+        now = 83
+
+    elif j == 6 and m >= 55 and m <= 0:
+        now = 84
+
+    elif j == 7 and m >= 0 and m <= 5:
+        now = 85
+
+    elif j == 7 and m >= 5 and m <= 10:
+        now = 86
+
+    elif j == 7 and m >= 10 and m <= 15:
+        now = 87
+
+    elif j == 7 and m >= 15 and m <= 20:
+        now = 88
+
+    elif j == 7 and m >= 20 and m <= 25:
+        now = 89
+
+    elif j == 7 and m >= 25 and m <= 30:
+        now = 90
+
+    elif j == 7 and m >= 30 and m <= 35:
+        now = 91
+
+    elif j == 7 and m >= 35 and m <= 40:
+        now = 92
+
+    elif j == 7 and m >= 40 and m <= 45:
+        now = 93
+
+    elif j == 7 and m >= 45 and m <= 50:
+        now = 94
+
+    elif j == 7 and m >= 50 and m <= 55:
+        now = 95
+
+    elif j == 7 and m >= 55 and m <= 0:
+        now = 96
+
+    elif j == 8 and m >= 0 and m <= 5:
+        now = 97
+
+    elif j == 8 and m >= 5 and m <= 10:
+        now = 98
+
+    elif j == 8 and m >= 10 and m <= 15:
+        now = 99
+
+    elif j == 8 and m >= 15 and m <= 20:
+        now = 100
+
+    elif j == 8 and m >= 20 and m <= 25:
+        now = 101
+
+    elif j == 8 and m >= 25 and m <= 30:
+        now = 102
+
+    elif j == 8 and m >= 30 and m <= 35:
+        now = 103
+
+    elif j == 8 and m >= 35 and m <= 40:
+        now = 104
+
+    elif j == 8 and m >= 40 and m <= 45:
+        now = 105
+
+    elif j == 8 and m >= 45 and m <= 50:
+        now = 106
+
+    elif j == 8 and m >= 50 and m <= 55:
+        now = 107
+
+    elif j == 8 and m >= 55 and m <= 0:
+        now = 108
+
+    elif j == 9 and m >= 0 and m <= 5:
+        now = 109
+
+    elif j == 9 and m >= 5 and m <= 10:
+        now = 110
+
+    elif j == 9 and m >= 10 and m <= 15:
+        now = 111
+
+    elif j == 9 and m >= 15 and m <= 20:
+        now = 112
+
+    elif j == 9 and m >= 20 and m <= 25:
+        now = 113
+
+    elif j == 9 and m >= 25 and m <= 30:
+        now = 114
+
+    elif j == 9 and m >= 30 and m <= 35:
+        now = 115
+
+    elif j == 9 and m >= 35 and m <= 40:
+        now = 116
+
+    elif j == 9 and m >= 40 and m <= 45:
+        now = 117
+
+    elif j == 9 and m >= 45 and m <= 50:
+        now = 118
+
+    elif j == 9 and m >= 50 and m <= 55:
+        now = 119
+
+    elif j == 9 and m >= 55 and m <= 0:
+        now = 120
+
+    elif j == 10 and m >= 0 and m <= 5:
+        now = 121
+
+    elif j == 10 and m >= 5 and m <= 10:
+        now = 122
+
+    elif j == 10 and m >= 10 and m <= 15:
+        now = 123
+
+    elif j == 10 and m >= 15 and m <= 20:
+        now = 124
+
+    elif j == 10 and m >= 20 and m <= 25:
+        now = 125
+
+    elif j == 10 and m >= 25 and m <= 30:
+        now = 126
+
+    elif j == 10 and m >= 30 and m <= 35:
+        now = 127
+
+    elif j == 10 and m >= 35 and m <= 40:
+        now = 128
+
+    elif j == 10 and m >= 40 and m <= 45:
+        now = 129
+
+    elif j == 10 and m >= 45 and m <= 50:
+        now = 130
+
+    elif j == 10 and m >= 50 and m <= 55:
+        now = 131
+
+    elif j == 10 and m >= 55 and m <= 0:
+        now = 132
+
+    elif j == 11 and m >= 0 and m <= 5:
+        now = 133
+
+    elif j == 11 and m >= 5 and m <= 10:
+        now = 134
+
+    elif j == 11 and m >= 10 and m <= 15:
+        now = 135
+
+    elif j == 11 and m >= 15 and m <= 20:
+        now = 136
+
+    elif j == 11 and m >= 20 and m <= 25:
+        now = 137
+
+    elif j == 11 and m >= 25 and m <= 30:
+        now = 138
+
+    elif j == 11 and m >= 30 and m <= 35:
+        now = 139
+
+    elif j == 11 and m >= 35 and m <= 40:
+        now = 140
+
+    elif j == 11 and m >= 40 and m <= 45:
+        now = 141
+
+    elif j == 11 and m >= 45 and m <= 50:
+        now = 142
+
+    elif j == 11 and m >= 50 and m <= 55:
+        now = 143
+
+    elif j == 11 and m >= 55 and m <= 0:
+        now = 144
+
+    elif j == 12 and m >= 0 and m <= 5:
+        now = 145
+
+    elif j == 12 and m >= 5 and m <= 10:
+        now = 146
+
+    elif j == 12 and m >= 10 and m <= 15:
+        now = 147
+
+    elif j == 12 and m >= 15 and m <= 20:
+        now = 148
+
+    elif j == 12 and m >= 20 and m <= 25:
+        now = 149
+
+    elif j == 12 and m >= 25 and m <= 30:
+        now = 150
+
+    elif j == 12 and m >= 30 and m <= 35:
+        now = 151
+
+    elif j == 12 and m >= 35 and m <= 40:
+        now = 152
+
+    elif j == 12 and m >= 40 and m <= 45:
+        now = 153
+
+    elif j == 12 and m >= 45 and m <= 50:
+        now = 154
+
+    elif j == 12 and m >= 50 and m <= 55:
+        now = 155
+
+    elif j == 12 and m >= 55 and m <= 0:
+        now = 156
+
+    elif j == 13 and m >= 0 and m <= 5:
+        now = 157
+
+    elif j == 13 and m >= 5 and m <= 10:
+        now = 158
+
+    elif j == 13 and m >= 10 and m <= 15:
+        now = 159
+
+    elif j == 13 and m >= 15 and m <= 20:
+        now = 160
+
+    elif j == 13 and m >= 20 and m <= 25:
+        now = 161
+
+    elif j == 13 and m >= 25 and m <= 30:
+        now = 162
+
+    elif j == 13 and m >= 30 and m <= 35:
+        now = 163
+
+    elif j == 13 and m >= 35 and m <= 40:
+        now = 164
+
+    elif j == 13 and m >= 40 and m <= 45:
+        now = 165
+
+    elif j == 13 and m >= 45 and m <= 50:
+        now = 166
+
+    elif j == 13 and m >= 50 and m <= 55:
+        now = 167
+
+    elif j == 13 and m >= 55 and m <= 0:
+        now = 168
+
+    elif j == 14 and m >= 0 and m <= 5:
+        now = 169
+
+    elif j == 14 and m >= 5 and m <= 10:
+        now = 170
+
+    elif j == 14 and m >= 10 and m <= 15:
+        now = 171
+
+    elif j == 14 and m >= 15 and m <= 20:
+        now = 172
+
+    elif j == 14 and m >= 20 and m <= 25:
+        now = 173
+
+    elif j == 14 and m >= 25 and m <= 30:
+        now = 174
+
+    elif j == 14 and m >= 30 and m <= 35:
+        now = 175
+
+    elif j == 14 and m >= 35 and m <= 40:
+        now = 176
+
+    elif j == 14 and m >= 40 and m <= 45:
+        now = 177
+
+    elif j == 14 and m >= 45 and m <= 50:
+        now = 178
+
+    elif j == 14 and m >= 50 and m <= 55:
+        now = 179
+
+    elif j == 14 and m >= 55 and m <= 0:
+        now = 180
+
+    elif j == 15 and m >= 0 and m <= 5:
+        now = 181
+
+    elif j == 15 and m >= 5 and m <= 10:
+        now = 182
+
+    elif j == 15 and m >= 10 and m <= 15:
+        now = 183
+
+    elif j == 15 and m >= 15 and m <= 20:
+        now = 184
+
+    elif j == 15 and m >= 20 and m <= 25:
+        now = 185
+
+    elif j == 15 and m >= 25 and m <= 30:
+        now = 186
+
+    elif j == 15 and m >= 30 and m <= 35:
+        now = 187
+
+    elif j == 15 and m >= 35 and m <= 40:
+        now = 188
+
+    elif j == 15 and m >= 40 and m <= 45:
+        now = 189
+
+    elif j == 15 and m >= 45 and m <= 50:
+        now = 190
+
+    elif j == 15 and m >= 50 and m <= 55:
+        now = 191
+
+    elif j == 15 and m >= 55 and m <= 0:
+        now = 192
+
+    elif j == 16 and m >= 0 and m <= 5:
+        now = 193
+
+    elif j == 16 and m >= 5 and m <= 10:
+        now = 194
+
+    elif j == 16 and m >= 10 and m <= 15:
+        now = 195
+
+    elif j == 16 and m >= 15 and m <= 20:
+        now = 196
+
+    elif j == 16 and m >= 20 and m <= 25:
+        now = 197
+
+    elif j == 16 and m >= 25 and m <= 30:
+        now = 198
+
+    elif j == 16 and m >= 30 and m <= 35:
+        now = 199
+
+    elif j == 16 and m >= 35 and m <= 40:
+        now = 200
+
+    elif j == 16 and m >= 40 and m <= 45:
+        now = 201
+
+    elif j == 16 and m >= 45 and m <= 50:
+        now = 202
+
+    elif j == 16 and m >= 50 and m <= 55:
+        now = 203
+
+    elif j == 16 and m >= 55 and m <= 0:
+        now = 204
+
+    elif j == 17 and m >= 0 and m <= 5:
+        now = 205
+
+    elif j == 17 and m >= 5 and m <= 10:
+        now = 206
+
+    elif j == 17 and m >= 10 and m <= 15:
+        now = 207
+
+    elif j == 17 and m >= 15 and m <= 20:
+        now = 208
+
+    elif j == 17 and m >= 20 and m <= 25:
+        now = 209
+
+    elif j == 17 and m >= 25 and m <= 30:
+        now = 210
+
+    elif j == 17 and m >= 30 and m <= 35:
+        now = 211
+
+    elif j == 17 and m >= 35 and m <= 40:
+        now = 212
+
+    elif j == 17 and m >= 40 and m <= 45:
+        now = 213
+
+    elif j == 17 and m >= 45 and m <= 50:
+        now = 214
+
+    elif j == 17 and m >= 50 and m <= 55:
+        now = 215
+
+    elif j == 17 and m >= 55 and m <= 0:
+        now = 216
+
+    elif j == 18 and m >= 0 and m <= 5:
+        now = 217
+
+    elif j == 18 and m >= 5 and m <= 10:
+        now = 218
+
+    elif j == 18 and m >= 10 and m <= 15:
+        now = 219
+
+    elif j == 18 and m >= 15 and m <= 20:
+        now = 220
+
+    elif j == 18 and m >= 20 and m <= 25:
+        now = 221
+
+    elif j == 18 and m >= 25 and m <= 30:
+        now = 222
+
+    elif j == 18 and m >= 30 and m <= 35:
+        now = 223
+
+    elif j == 18 and m >= 35 and m <= 40:
+        now = 224
+
+    elif j == 18 and m >= 40 and m <= 45:
+        now = 225
+
+    elif j == 18 and m >= 45 and m <= 50:
+        now = 226
+
+    elif j == 18 and m >= 50 and m <= 55:
+        now = 227
+
+    elif j == 18 and m >= 55 and m <= 0:
+        now = 228
+
+    elif j == 19 and m >= 0 and m <= 5:
+        now = 229
+
+    elif j == 19 and m >= 5 and m <= 10:
+        now = 230
+
+    elif j == 19 and m >= 10 and m <= 15:
+        now = 231
+
+    elif j == 19 and m >= 15 and m <= 20:
+        now = 232
+
+    elif j == 19 and m >= 20 and m <= 25:
+        now = 233
+
+    elif j == 19 and m >= 25 and m <= 30:
+        now = 234
+
+    elif j == 19 and m >= 30 and m <= 35:
+        now = 235
+
+    elif j == 19 and m >= 35 and m <= 40:
+        now = 236
+
+    elif j == 19 and m >= 40 and m <= 45:
+        now = 237
+
+    elif j == 19 and m >= 45 and m <= 50:
+        now = 238
+
+    elif j == 19 and m >= 50 and m <= 55:
+        now = 239
+
+    elif j == 19 and m >= 55 and m <= 0:
+        now = 240
+
+    elif j == 20 and m >= 0 and m <= 5:
+        now = 241
+
+    elif j == 20 and m >= 5 and m <= 10:
+        now = 242
+
+    elif j == 20 and m >= 10 and m <= 15:
+        now = 243
+
+    elif j == 20 and m >= 15 and m <= 20:
+        now = 244
+
+    elif j == 20 and m >= 20 and m <= 25:
+        now = 245
+
+    elif j == 20 and m >= 25 and m <= 30:
+        now = 246
+
+    elif j == 20 and m >= 30 and m <= 35:
+        now = 247
+
+    elif j == 20 and m >= 35 and m <= 40:
+        now = 248
+
+    elif j == 20 and m >= 40 and m <= 45:
+        now = 249
+
+    elif j == 20 and m >= 45 and m <= 50:
+        now = 250
+
+    elif j == 20 and m >= 50 and m <= 55:
+        now = 251
+
+    elif j == 20 and m >= 55 and m <= 0:
+        now = 252
+
+    elif j == 21 and m >= 0 and m <= 5:
+        now = 253
+
+    elif j == 21 and m >= 5 and m <= 10:
+        now = 254
+
+    elif j == 21 and m >= 10 and m <= 15:
+        now = 255
+
+    elif j == 21 and m >= 15 and m <= 20:
+        now = 256
+
+    elif j == 21 and m >= 20 and m <= 25:
+        now = 257
+
+    elif j == 21 and m >= 25 and m <= 30:
+        now = 258
+
+    elif j == 21 and m >= 30 and m <= 35:
+        now = 259
+
+    elif j == 21 and m >= 35 and m <= 40:
+        now = 260
+
+    elif j == 21 and m >= 40 and m <= 45:
+        now = 261
+
+    elif j == 21 and m >= 45 and m <= 50:
+        now = 262
+
+    elif j == 21 and m >= 50 and m <= 55:
+        now = 263
+
+    elif j == 21 and m >= 55 and m <= 0:
+        now = 264
+
+    elif j == 22 and m >= 0 and m <= 5:
+        now = 265
+
+    elif j == 22 and m >= 5 and m <= 10:
+        now = 266
+
+    elif j == 22 and m >= 10 and m <= 15:
+        now = 267
+
+    elif j == 22 and m >= 15 and m <= 20:
+        now = 268
+
+    elif j == 22 and m >= 20 and m <= 25:
+        now = 269
+
+    elif j == 22 and m >= 25 and m <= 30:
+        now = 270
+
+    elif j == 22 and m >= 30 and m <= 35:
+        now = 271
+
+    elif j == 22 and m >= 35 and m <= 40:
+        now = 272
+
+    elif j == 22 and m >= 40 and m <= 45:
+        now = 273
+
+    elif j == 22 and m >= 45 and m <= 50:
+        now = 274
+
+    elif j == 22 and m >= 50 and m <= 55:
+        now = 275
+
+    elif j == 22 and m >= 55 and m <= 0:
+        now = 276
+
+    elif j == 23 and m >= 0 and m <= 5:
+        now = 277
+
+    elif j == 23 and m >= 5 and m <= 10:
+        now = 278
+
+    elif j == 23 and m >= 10 and m <= 15:
+        now = 279
+
+    elif j == 23 and m >= 15 and m <= 20:
+        now = 280
+
+    elif j == 23 and m >= 20 and m <= 25:
+        now = 281
+
+    elif j == 23 and m >= 25 and m <= 30:
+        now = 282
+
+    elif j == 23 and m >= 30 and m <= 35:
+        now = 283
+
+    elif j == 23 and m >= 35 and m <= 40:
+        now = 284
+
+    elif j == 23 and m >= 40 and m <= 45:
+        now = 285
+
+    elif j == 23 and m >= 45 and m <= 50:
+        now = 286
+
+    elif j == 23 and m >= 50 and m <= 55:
+        now = 287
+
+    elif j == 23 and m >= 55 and m <= 0:
+        now = 288
+
+    return now
