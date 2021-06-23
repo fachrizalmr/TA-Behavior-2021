000fc73c4cbae8a5aca323ba90a8d7e059a50cdf
Muhammad Fachrizal Ramdani <32020922+fachrizalmr@users.noreply.github.com>
2021-06-23 19:22:24 +0800

    new

---
+++ Webcam/recognizeWajah.py
@@ -0,0 +1,45 @@
+import cv2
+import os
+wajahDir = 'dataWajah'
+latihDir = 'latihWajah'
+cam = cv2.VideoCapture(1)
+cam.set(3, 640)
+cam.set(4, 480)
+faceDetector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faceRecognizer = cv2.face.LBPHFaceRecognizer_create()
+
+faceRecognizer.read(latihDir+'/training.xml')
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+id = 0
+names = ['Tidak Diketahui', 'Fachrizal', 'Intruder']
+minWidth = 0.1*cam.get(3)
+minHeight = 0.1*cam.get(4)
+
+while True:
+    retV, frame = cam.read()
+    frame = cv2.flip(frame, 1)  # vertical flip
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceDetector.detectMultiScale(
+        grey, 1.2, 5, minSize=(round(minWidth), round(minHeight)),)
+    for (x, y, w, h) in faces:
+        frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        id, confidence = faceRecognizer.predict(grey[y:y+h, x:x+w])
+        if confidence <= 50:
+            nameID = names[id]
+            confidenceTxt = " {0}%".format(round(100-confidence))
+        else:
+            nameID = names[0]
+            confidenceTxt = " {0}%".format(round(100-confidence))
+        cv2.putText(frame, str(nameID), (x+5, y-5),
+                    font, 1, (255, 255, 255), 2)
+        cv2.putText(frame, str(confidenceTxt), (x+5, y+h-5),
+                    font, 1, (255, 255, 0), 1)
+
+    cv2.imshow('Recognisi Wajah', frame)
+    k = cv2.waitKey(1) & 0xFF == ord('q')
+    if k == 27 or k == ord('q'):
+        break
+print("Exit")
+cam.release()
+cv2.destroyAllWindows()
---
+++ Webcam/rekamWajah.py
@@ -0,0 +1,36 @@
+import cv2
+import os
+wajahDir = 'dataWajah'
+cam = cv2.VideoCapture(1)
+cam.set(3, 640)
+cam.set(4, 480)
+faceDetector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eyeDetector = cv2.CascadeClassifier('haarcascade_eye.xml')
+faceID = input(
+    "Masukkan Face ID yang akan di rekam Dataya [Kemudian Tekan Enter]: ")
+print("Tatap wajah ke depan Anda ke dalam webcam. Tunggu proses pengambilan data wajah selesai.")
+ambilData = 1
+while True:
+    retV, frame = cam.read()
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceDetector.detectMultiScale(grey, 1.3, 5)
+    for (x, y, w, h) in faces:
+        frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        namaFile = 'wajah.'+str(faceID)+'.'+str(ambilData)+'.jpg'
+        cv2.imwrite(wajahDir+'/'+namaFile, frame)
+        ambilData += 1
+        roigrey = grey[y:y+h, x:x+w]
+        roiWarna = frame[y:y+h, x:x+w]
+        eyes = eyeDetector.detectMultiScale(roigrey)
+        for (xe, ye, we, he) in eyes:
+            cv2.rectangle(roiWarna, (xe, ye), (xe+we, ye+he), (0, 0, 255), 1)
+
+    cv2.imshow('MyWebcam', frame)
+    k = cv2.waitKey(1) & 0xFF == ord('q')
+    if k == 27 or k == ord('q'):
+        break
+    elif ambilData > 30:
+        break
+print("Pengambilan Data Selesai")
+cam.release()
+cv2.destroyAllWindows()
